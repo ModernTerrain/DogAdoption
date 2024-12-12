450db7b8803c0993888562aaae8e7f8e916b6d50
Ben Newland <benjamin.newland@ndsu.edu>
2024-12-11 22:45:26 -0600

    Added the Dog model, as well as the default razor pages related to it. Added functionality to search dogs by name, age, breed, size, adoption status, hypoallergenic status, and adoption fee.

---
+++ Migrations/20241212023557_test migration.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DogAdoption.Migrations
+{
+    /// <inheritdoc />
+    public partial class testmigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Dog",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(60)", maxLength: 60, nullable: false),
+                    Age = table.Column<int>(type: "int", nullable: false),
+                    Breed = table.Column<string>(type: "nvarchar(60)", maxLength: 60, nullable: false),
+                    Size = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(500)", maxLength: 500, nullable: false),
+                    Hypoallergenic = table.Column<bool>(type: "bit", nullable: false),
+                    ImageFileName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Status = table.Column<string>(type: "nvarchar(60)", maxLength: 60, nullable: false),
+                    AdoptionFee = table.Column<decimal>(type: "decimal(18,2)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Dog", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Dog");
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,6 +1,15 @@
-using DogAdoption.Components;
+﻿using DogAdoption.Components;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using DogAdoption.Data;
 
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddDbContextFactory<DogAdoptionContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DogAdoptionContext") ?? throw new InvalidOperationException("Connection string 'DogAdoptionContext' not found.")));
+
+builder.Services.AddQuickGridEntityFrameworkAdapter();
+
+builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
 // Add services to the container.
 builder.Services.AddRazorComponents()
@@ -14,6 +23,7 @@ if (!app.Environment.IsDevelopment())
     app.UseExceptionHandler("/Error", createScopeForErrors: true);
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
+    app.UseMigrationsEndPoint();
 }
 
 app.UseHttpsRedirection();
