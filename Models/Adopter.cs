afc6903afd1505d7f78af22064262ad582118ad2
nickh <nicholas.hoedl@ndsu.edu>
2024-12-16 18:28:40 -0600

    Implements most of the feedback sending functionality

--- Models/Adopter.cs
+++ Models/Adopter.cs
@@ -2,5 +2,6 @@
 {
     public class Adopter
     {
+        public string? Name { get; set; }
     }
 }
---
+++ Models/Message.cs
@@ -0,0 +1,13 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace DogAdoption.Models
+{
+    public class Message
+    {
+        public int Id { get; set; }
+        public string? RecipientName { get; set; }  
+        public string? SenderName { get; set; }
+        public string? Content {  get; set; }
+
+    }
+}
